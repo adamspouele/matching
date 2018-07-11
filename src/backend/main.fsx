11b39ccd3f96212a12ebb1556a14563097a6670e
adams003 <adamspouele@gmail.com>
2018-07-11 17:59:14 +0200

    update

--- src/backend/main.fsx
+++ src/backend/main.fsx
@@ -1,5 +1,7 @@
 #r "../../Suave.2.4.3/lib/net461/Suave.dll"
 
+
+open Suave.State.CookieStateStore
 open Suave 
 open Suave.Json
 open System.Runtime.Serialization
@@ -7,21 +9,24 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 
-
 //
 
 let app =
   choose
-    [ GET >=> choose
+    [ 
+      path "/websocket" >=> handShake ws
+      GET >=> choose
         [ 
           path "/hello" >=> OK "Hello GET"
           path "/goodbye" >=> OK "Good bye GET" 
+          RequestErrors.NOT_FOUND "404 not Found."
         ]
       POST >=> choose
         [ 
           path "/hello" >=> OK "Hello POST"
           path "/goodbye" >=> OK "Good bye POST" 
-        ] 
+          RequestErrors.NOT_FOUND "404 not Found."
+        ]
     ]
 
 
