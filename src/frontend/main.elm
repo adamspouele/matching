d99eb78a17f35d6fd220fd9e52a44a61bac722ac
JorisNT <jorisnsenguet@gmail.com>
2018-07-13 16:11:53 +0200

    starting http request

--- src/frontend/main.elm
+++ src/frontend/main.elm
@@ -1,17 +1,40 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as JD
+import Json.Encode as JE
 
 
 main =
   Html.beginnerProgram { model = init, view = view, update = update }
 
+api =
+    "https://61d9262f.ngrok.io/"
+
+mu : String -> String
+mu url =
+    (api ++ url)
+
+createPlayer : Http.Request User
+createPlayer =
+    Http.get (mu "player") userDecoder
+
+userDecoder : JD.Decoder User
+userDecoder =
+    JD.map2 User
+        (JD.field "id" JD.int)
+        (JD.field "name" JD.string)
 
 -- MODEL
+type alias User = {
+    id: Int
+    ,name: String
+}
 
 type alias Card = {
-    cardId: Int
+    cardId: String,
+    isDiscover: Bool
 }
 
 type alias Player = {
@@ -46,21 +69,27 @@ initOpponent =
     , marks = 0
     }
 
+initCard: String -> Card
+initCard cardId =
+    { cardId = cardId
+    , isDiscover = False
+    }
+
 init : Game
 init = 
     { cards =
-        [ Card 0
-        , Card 1
-        , Card 2
-        , Card 3
-        , Card 4
-        , Card 5
-        , Card 6
-        , Card 7
-        , Card 8
-        , Card 9
-        , Card 10
-        , Card 11
+        [ initCard "0"
+        , initCard "1"
+        , initCard "2"
+        , initCard "3"
+        , initCard "4"
+        , initCard "5"
+        , initCard "2a"
+        , initCard "4a"
+        , initCard "0a"
+        , initCard "3a"
+        , initCard "1a"
+        , initCard "5a"
         ]
     , player = initPlayer
     , opponent = initOpponent
@@ -68,40 +97,79 @@ init =
 
 -- UPDATE
 
-type Msg
-    = IdCard Int
+update : String -> Game -> Game
+update cardId game =
+    selectCard cardId game
 
-update : Msg -> Game -> Game
-update msg game =
-    case msg of 
-        IdCard id ->
-            selectCard id game
-
-selectCard : Int -> Game -> Game
+selectCard : String -> Game -> Game
 selectCard idCard game =
     if List.length game.player.openedCards == 2 then
         game
     else
-        let op = 
-                List.append game.player.openedCards (getCard idCard game)
+        let card =
+            (getCard idCard game.cards)
+            openedCard =
+                {card | isDiscover = True}
+            op = 
+                List.append game.player.openedCards [openedCard]
             thePlayer = game.player
             nPlayer =
                 {thePlayer | openedCards = op}
         in
             ({game | player = nPlayer})
 
-isTheCard : Int -> Card -> Bool
+isTheCard : String -> Card -> Bool
 isTheCard id card =
     if card.cardId == id then True else False
 
-getCard : Int -> Game -> List Card
-getCard idCard game =
-    List.filter (isTheCard idCard) game.cards
+getCard : String -> List Card -> Card
+getCard idCard cards =
+    let card =
+        List.head (List.filter (isTheCard idCard) cards)
+    in (
+        case card of
+            Nothing -> (initCard idCard)
+            Just val -> val
+    )
 
 -- VIEW
 
+getLink : Card -> String
+getLink card =
+    if card.isDiscover then card.cardId
+    else "cover"
 
-view : Game -> Html Msg
+linkToCard : List Card -> Card -> String
+linkToCard openedCards card =
+    if ((List.length openedCards) > 0) then
+    (
+        if card.isDiscover then card.cardId
+        else (
+            let foundCard =
+                getCard card.cardId openedCards
+            in (
+                getLink foundCard
+            )
+        )
+    )
+    else
+    (
+        getLink card
+    )
+
+cardToHtml : List Card -> Card -> Html String
+cardToHtml openedCards card =
+    img[
+    style[("width", "116px"), ("height", "160px")]
+    , src ("./cards/" ++ (linkToCard openedCards card) ++ ".png")
+    , onClick card.cardId] []
+
+printAllCards : List Card -> List Card -> Html String
+printAllCards gameCards openedCards =
+    div[style[("width", "464px"), ("margin", "auto")]]
+        (List.map (cardToHtml openedCards) gameCards)
+
+view : Game -> Html String
 view game =
     let player =
             game.player
@@ -112,4 +180,5 @@ view game =
         [ div[][text player.name]
         , div[][text ("Your Score: " ++ (toString player.marks))]
         , div[][text (opponent.name ++ " Score: " ++ (toString opponent.marks))]
+        , printAllCards game.cards game.player.openedCards
         ])
